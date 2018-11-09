Updated gradients to include the regularization the correct number of times.

cost.py
@@ -1,14 +1,15 @@
 import numpy as np
 
+
 class Cost(object):
 
     def __init__(self, n_classes):
         self.n_classes = n_classes
 
-    def forward(self):
+    def forward(self, predictions, y, params, lambdas):
         raise NotImplementedError
 
-    def backward(self):
+    def backward(self, predictions, y):
         raise NotImplementedError
 
 
@@ -20,9 +21,9 @@ class LogCost(Cost):
         onehot[y, np.arange(n_predictions)] = 1
         cost = np.sum(np.multiply(predictions, onehot), axis=0)
         cost = -np.log(cost) + lambdas[0]*np.sum(np.sum(np.abs(params['w1']))) + \
-               lambdas[1]*np.sum(np.sum(np.multiply(params['w1'], params['w1']))) + \
+               lambdas[1]*np.sum(np.sum(np.square(params['w1']))) + \
                lambdas[2]*np.sum(np.sum(np.abs(params['w2']))) + \
-               lambdas[3]*np.sum(np.sum(np.multiply(params['w2'], params['w2'])))
+               lambdas[3]*np.sum(np.sum(np.square(params['w2'])))
         return cost
 
     def backward(self, predictions, y):

main.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 def gradient_check(parameter, mlp, perturbation, x, y):
     mlp.forward_pass(x)
     mlp.calculate_cost(y)
-    mlp.backward_pass(x, y)
     cost = mlp.fprop['cost']
+    mlp.backward_pass(x, y)
     numerical_gradient = np.zeros((mlp.params[parameter].shape[0], mlp.params[parameter].shape[1], len(cost)))
     rows, columns = mlp.params[parameter].shape
     for i in range(rows):
@@ -23,21 +23,27 @@ def gradient_check(parameter, mlp, perturbation, x, y):
     check = np.divide(numerical_gradient+perturbation*0.001, mlp.bprop['grad_{}'.format(parameter)]+perturbation*0.001)
     return numerical_gradient, check
 
-def plot_decision_boundary(clf, x, y):
-    x_min, x_max = x[0, :].min() - 1, x[0, :].max() + 1
-    y_min, y_max = x[1, :].min() - 1, x[1, :].max() + 1
+
+def plot_decision_boundary(clf, x, y, title, save_name):
+    fig, ax = plt.subplots()
+    x_min, x_max = x[0, :].min() - 0.25, x[0, :].max() + 0.25
+    y_min, y_max = x[1, :].min() - 0.25, x[1, :].max() + 0.25
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
     Z = clf.predict(np.transpose(np.c_[xx.ravel(), yy.ravel()]))
-    print(Z.shape)
-    print(np.sum(Z))
     Z = Z.reshape(xx.shape)
-    cs = plt.contourf(xx, yy, Z, alpha=0.4, cmap=plt.cm.bwr)
-    plt.scatter(x[0, :], x[1, :], c=y.ravel(), s=2, cmap=plt.cm.spring)
-    plt.suptitle("Decision surface of a decision tree using paired features")
-    plt.legend(loc='lower right', borderpad=0, handletextpad=0)
+    plt.contourf(xx, yy, Z, alpha=0.4, cmap=plt.cm.bwr)
+    unique_classes = np.unique(y)
+    colors = plt.cm.spring(np.linspace(0, 1, len(unique_classes)))
+    for i, class_ in enumerate(unique_classes):
+        _, mask = np.where(y == class_)
+        ax.scatter(x[0, mask], x[1, mask], c=colors[i], label='Category: {}'.format(class_),
+                   s=3, edgecolors='black', linewidths=0.2)
+    ax.legend(loc='upper right', framealpha=1)
+    ax.grid(True)
+    plt.title(title)
     plt.axis("tight")
+    plt.savefig(save_name)
     plt.show()
-
 
 
 def experiment_1():
@@ -66,15 +72,16 @@ def experiment_1():
         'numerical_gradient_b1': numerical_gradient_b1,
         'grad_b1': mlp.bprop['grad_b1'],
         'check_b1': check_b1}
-    np.save(os.path.join('results', 'experiment_1.npy'), experiment_1)
+    with open(os.path.join('results', 'experiment1', 'experiment1.txt'), 'w') as file:
+        file.write(str(experiment_1))
 
 
 def experiment_2():
     inputs = 2
     outputs = 3
     hidden = 3
-    n = 1
-    lambdas = [0.1, 0.1, 0.1, 0.1]
+    n = 2
+    lambdas = [0.19, 0.45, 0.12, 0.3]
     mlp = MLP(inputs, hidden, outputs, lambdas)
     x = np.random.rand(inputs, n)
     y = np.random.randint(0, outputs, (1, n))
@@ -95,22 +102,79 @@ def experiment_2():
         'numerical_gradient_b1': numerical_gradient_b1,
         'grad_b1': mlp.bprop['grad_b1'],
         'check_b1': check_b1}
-    np.save(os.path.join('results', 'experiment_1.npy'), experiment_2)
+    with open(os.path.join('results', 'experiment2', 'experiment2.txt'), 'w') as file:
+        file.write(str(experiment_2))
+
 
 def experiment_5():
-    # Circle data
+    # Load data
     circle = np.loadtxt(os.path.join('data', 'circle.txt'))
+    ellipse = np.loadtxt(os.path.join('data', 'ellipse.txt'))
+    # constants
+    save_dir = os.path.join('results', 'experiment5')
+
+    # Base case
+    lr = 0.05
+    lambdas = [0, 0, 0, 0]
+    hidden = 3
+    epochs = 15
+    batch_size = 20
+    # Circle data
     x = circle[:, :-1].T
     y = np.clip(circle[:, -1].T.reshape(1, -1), 0, 1).astype(int)
     inputs = len(x)
     outputs = len(np.unique(y))
-    hidden = 5000
-    mlp = MLP(inputs, hidden, outputs, [0, 0, 0, 0])
-    mlp.fit(x, y, 10, 100, 0.001)
-    plot_decision_boundary(mlp, x, y)
+    mlp = MLP(inputs, hidden, outputs, lambdas)
+    mlp.fit(x, y, batch_size, epochs, lr)
+    name = os.path.join(save_dir, 'base_case_circle')
+    plot_decision_boundary(mlp, x, y, 'Decision surface on an MLP for circle data', name)
+    # Ellipse data
+    x = ellipse[:, :-1].T
+    y = np.clip(circle[:, -1].T.reshape(1, -1), 0, 1).astype(int)
+    inputs = len(x)
+    outputs = len(np.unique(y))
+    mlp = MLP(inputs, hidden, outputs, lambdas)
+    mlp.fit(x, y, batch_size, epochs, lr)
+    name = os.path.join(save_dir, 'base_case_ellipse')
+    plot_decision_boundary(mlp, x, y, 'Decision surface on an MLP for ellipse data', name)
+
+    # Varying weight decay
+    lr = 0.01
+    lambdas = [[0.0, 0.0, 0.0, 0.0],
+               [0.3, 0.3, 0.3, 0.3],
+               [0.8, 0.0, 0.8, 0.0],
+               [0.0, 0.8, 0.0, 0.8]]
+    hidden = 50
+    epochs = 5
+    batch_size = 100
+    for i in lambdas:
+        # Circle data
+        x = circle[:, :-1].T
+        y = np.clip(circle[:, -1].T.reshape(1, -1), 0, 1).astype(int)
+        inputs = len(x)
+        outputs = len(np.unique(y))
+        mlp = MLP(inputs, hidden, outputs, i)
+        mlp.fit(x, y, batch_size, epochs, lr)
+        name = os.path.join(save_dir, 'lr', 'circle_lr_{}'.format(i)).replace(".", "")
+        plot_decision_boundary(mlp, x, y, 'Decision surface on an MLP for circle data with lr: {}'.format(i), name)
+        # # Ellipse data
+        # x = ellipse[:, :-1].T
+        # y = np.clip(circle[:, -1].T.reshape(1, -1), 0, 1).astype(int)
+        # inputs = len(x)
+        # outputs = len(np.unique(y))
+        # mlp = MLP(inputs, hidden, outputs, i)
+        # mlp.fit(x, y, batch_size, epochs, lr)
+        # name = os.path.join(save_dir, 'lr', 'ellipse_lr_{}'.format(i)).replace(".", "")
+        # plot_decision_boundary(mlp, x, y, 'Decision surface on an MLP for ellipse data with lr: {}'.format(i), name)
+
+
+def experiment_10():
+    pass
+
 
 if __name__ == '__main__':
     # EXPERIMENTS
 
-    experiment_5()
+    experiment_2()
+
 

mlp.py
@@ -63,9 +63,12 @@ class MLP(object):
         self.bprop['grad_b2'] = grad_b2
 
         # Gradient of loss wrt weights 2
+        # The penalization has to be added the same number of times as the number of examples!
+        reps = grad_oa.shape[-1]
         grad_w2 = np.matmul(grad_oa, np.transpose(self.fprop['hs']))
-        self.bprop['grad_w2'] = grad_w2 + \
-                                self.lambdas[2]*np.sign(self.params['w2']) + self.lambdas[3]*2*(self.params['w2'])
+        grad_w2_pen = reps*(self.lambdas[2]*np.sign(self.params['w2']) + self.lambdas[3]*2*(self.params['w2']))
+        self.bprop['grad_w2'] = grad_w2 + grad_w2_pen
+
 
         # Gradient of loss wrt hs
         grad_hs = np.matmul(np.transpose(self.params['w2']), grad_oa)
@@ -83,9 +86,10 @@ class MLP(object):
         self.bprop['grad_b1'] = grad_b1
 
         # Gradient of loss wrt weights 1
+        # The penalization has to be added the same number of times as the number of examples!
         grad_w1 = np.matmul(grad_ha, np.transpose(x))
-        self.bprop['grad_w1'] = grad_w1 + \
-                                self.lambdas[0]*np.sign(self.params['w1']) + self.lambdas[1]*2*(self.params['w1'])
+        grad_w1_pen = reps*(self.lambdas[0]*np.sign(self.params['w1']) + self.lambdas[1]*2*(self.params['w1']))
+        self.bprop['grad_w1'] = grad_w1 + grad_w1_pen
 
     def update_weights(self, eta):
         for key in self.params.keys():
@@ -117,7 +121,6 @@ class MLP(object):
 
     def predict(self, x):
         prediction = self.forward_pass(x)
-        print(prediction)
         prediction = np.argmax(prediction, axis=0)
         return prediction
 